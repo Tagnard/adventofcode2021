7bcd7bc675f785206aa6a61e5d04284c6885aa85
Emil Haugbergsmyr <emil@raeven.net>
2021-12-02 11:42:10 +0100

    Day 2 - completed

--- src/lib.rs
+++ src/lib.rs
@@ -93,3 +93,67 @@ fn verify_day_1_part_2() {
     ), 5);
 }
 
+pub fn day_2_part_1(input: Vec<String>) -> i64 {
+    let mut position: (i64, i64) = (0, 0);
+
+    for line in input {
+        let parts: Vec<&str> = line.split(" ").collect();
+        let direction = parts[0];
+        let amount = parts[1].parse::<i64>().unwrap();
+
+        match direction {
+            "up" => position.1 = position.1 - amount,
+            "down" => position.1 = position.1 + amount,
+            "forward" => position.0 = position.0 + amount,
+            _  => (),
+        };
+    }
+
+    position.0 * position.1
+}
+
+#[test]
+fn verify_day_2_part_1() {
+    assert_eq!(day_2_part_1(vec![
+        "forward 5".to_owned(), 
+        "down 5".to_owned(), 
+        "forward 8".to_owned(), 
+        "up 3".to_owned(), 
+        "down 8".to_owned(), 
+        "forward 2".to_owned()]), 150);
+}
+
+pub fn day_2_part_2(input: Vec<String>) -> i64 {
+    let mut horizontal_position: i64 = 0;
+    let mut depth: i64 = 0;
+    let mut aim: i64 = 0;
+
+    for line in input {
+        let parts: Vec<&str> = line.split(" ").collect();
+        let direction = parts[0];
+        let amount = parts[1].parse::<i64>().unwrap();
+
+        match direction {
+            "up" => aim = aim - amount,
+            "down" => aim = aim + amount,
+            "forward" => {
+                horizontal_position = horizontal_position + amount;
+                depth = depth + amount * aim;
+            },
+            _  => (),
+        };
+    }
+
+    horizontal_position * depth
+}
+
+#[test]
+fn verify_day_2_part_2() {
+    assert_eq!(day_2_part_2(vec![
+        "forward 5".to_owned(), 
+        "down 5".to_owned(), 
+        "forward 8".to_owned(), 
+        "up 3".to_owned(), 
+        "down 8".to_owned(), 
+        "forward 2".to_owned()]), 900);
+}
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,6 @@ use aoc_2021::*;
 fn main() {
     println!("Day 1 - Part 1: {}", day_1_part_1(get_lines("input_day_1_part_1.txt")));
     println!("Day 1 - Part 2: {}", day_1_part_2(get_lines("input_day_1_part_2.txt")));
+    println!("Day 2 - Part 1: {}", day_2_part_1(get_lines("input_day_2_part_1.txt")));
+    println!("Day 2 - Part 2: {}", day_2_part_2(get_lines("input_day_2_part_2.txt")));
 }
