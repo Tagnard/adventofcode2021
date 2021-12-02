6edefe36974ae6c713b88a86ee23ae39a9d3005d
Emil Haugbergsmyr <emil@raeven.net>
2021-12-02 11:49:47 +0100

    Moved all input files to its own directory

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use aoc_2021::*;
 
 fn main() {
-    println!("Day 1 - Part 1: {}", day_1_part_1(get_lines("input_day_1_part_1.txt")));
-    println!("Day 1 - Part 2: {}", day_1_part_2(get_lines("input_day_1_part_2.txt")));
-    println!("Day 2 - Part 1: {}", day_2_part_1(get_lines("input_day_2_part_1.txt")));
-    println!("Day 2 - Part 2: {}", day_2_part_2(get_lines("input_day_2_part_2.txt")));
+    println!("Day 1 - Part 1: {}", day_1_part_1(get_lines("./input/day_1_part_1.txt")));
+    println!("Day 1 - Part 2: {}", day_1_part_2(get_lines("./input/day_1_part_2.txt")));
+    println!("Day 2 - Part 1: {}", day_2_part_1(get_lines("./input/day_2_part_1.txt")));
+    println!("Day 2 - Part 2: {}", day_2_part_2(get_lines("./input/day_2_part_2.txt")));
 }
